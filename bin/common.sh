05be4cf93a43675388436f15151532daae9c9ece
Jacob Rosenberg <jake@lendup.com>
2014-03-25 17:04:27 -0700

    point to LendUp's repo, not heroku's

--- bin/common.sh
+++ bin/common.sh
@@ -24,7 +24,7 @@ check_compile_status()
 install_play()
 {
   VER_TO_INSTALL=$1
-  PLAY_URL="https://s3.amazonaws.com/heroku-jvm-langpack-play/play-heroku-$VER_TO_INSTALL.tar.gz"
+  PLAY_URL="https://s3.amazonaws.com/lendup.packages/play-modules/play-heroku-$VER_TO_INSTALL.tar.gz"
   PLAY_TAR_FILE="play-heroku.tar.gz"
   echo "-----> Installing Play! $VER_TO_INSTALL....."
   curl --silent --max-time 150 --location $PLAY_URL -o $PLAY_TAR_FILE
