f9e53a3311074bcfdc5a247d1daaf8a39a20f5c6
Joe Kutner <jpkutner@gmail.com>
2015-01-21 13:06:47 -0600

    updated publish script to be like other java buildpacks [skip ci]

--- etc/publish.sh
+++ etc/publish.sh
@@ -1,29 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 BP_NAME="play"
 
 if [ ! -z "$1" ]; then
-  rm -rf /tmp/heroku-buildpack-$BP_NAME
   pushd . > /dev/null 2>&1
-  cd /tmp
+  cd /tmp &&
   git clone git@github.com:heroku/heroku-buildpack-$BP_NAME.git
   cd heroku-buildpack-$BP_NAME
   git checkout master
-  find . ! -name '.' ! -name '..' ! -name 'bin' -maxdepth 1 -print0 | xargs -0 rm -rf --
+  headHash=$(git rev-parse HEAD)
+
+  find . ! -name '.' ! -name '..' ! -name 'bin' ! -name 'opt' -maxdepth 1 -print0 | xargs -0 rm -rf --
   heroku buildpacks:publish $1/$BP_NAME
 
   if [ "$1" = "heroku" ]; then
-    echo "Tagging commit... "
     newTag=$(heroku buildpacks:revisions heroku/$BP_NAME | sed -n 2p | grep -o -e "v\d*")
-    git tag $newTag
-    git push --tags
   fi
 
   popd > /dev/null 2>&1
   echo "Cleaning up..."
   rm -rf /tmp/heroku-buildpack-$BP_NAME
+
+  if [ "$1" = "heroku" ]; then
+    if [ "$headHash" = "$(git rev-parse HEAD)" ]; then
+      echo "Tagging commit $headHash with $newTag... "
+      git tag $newTag
+      git push --tags
+    fi
+  fi
+
   echo "Done."
 else
   echo "You must provide a buildkit organization as an argument!"
